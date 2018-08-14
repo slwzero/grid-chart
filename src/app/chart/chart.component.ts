c25d0c8146b7c8c728554b790b3807269d9b4a8c
slwzero <slwzero2498@gmail.com>
2018-08-15 01:57:37 +0800

    fix

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  selector: "app-chart",
+  templateUrl: "./chart.component.html",
+  styleUrls: ["./chart.component.scss"]
 })
 export class ChartComponent implements OnInit {
-  @Input() mockData;
-  constructor() { }
+  @Input()
+  mockData;
+  constructor() {}
   totalBars = 50; // 一共50个小格子
   newMockData: any;
   ngOnInit() {
@@ -61,40 +62,44 @@ export class ChartComponent implements OnInit {
   //   console.log(bars);
   // }
   setBar() {
-  // [10.5, 20.3, 19.2]
-  const barsAssign = this.newMockData.propertion.map(ele => {
-  return ele.num / ele.total * this.totalBars;
-  });
-  let bars = []; // [10, 20, 19];
-  const points = []; // [0.5, 0.3, 0.2];
-  let sum = 0;
+    // [10.5, 20.3, 19.2]
+    const barsAssign = this.newMockData.propertion.map(ele => {
+      return (ele.num / ele.total) * this.totalBars;
+    });
+    let bars = []; // [10, 20, 19];
+    const points = []; // [0.5, 0.3, 0.2];
+    let sum = 0;
 
-  barsAssign.forEach(ele => {
-  const integer = Math.floor(ele);
-  points.push(ele - integer);
-  bars.push(integer);
-  sum += integer;
-  });
+    barsAssign.forEach(ele => {
+      let integer = Math.floor(ele);
+      points.push(ele - integer);
+      if (integer === 0) {
+        integer = 1;
+      }
+      bars.push(integer);
+      sum += integer;
+    });
 
-  while (sum < this.totalBars) {
-  // 获取最大的小数位在points的索引值;进1位，
-  const maxPoint = Math.max(...points);
-  const index = points.indexOf(maxPoint);
-  bars[index] = bars[index] + 1; // [11, 20, 19];
-  sum++;
-  }
+    while (sum < this.totalBars) {
+      // 获取最大的小数位在points的索引值;进1位，
+      const maxPoint = Math.max(...points);
+      const index = points.indexOf(maxPoint);
+      bars[index] = bars[index] + 1; // [11, 20, 19];
+      points[index] = 0; // 最大points对应的bars的加1之后，就置为0，下次就不会再加。
+      sum++;
+    }
 
-  // angular想遍历数字 ===》要通过转成数组方式
-  bars = bars.map(ele => {
-  const temp = [];
-  for (let i = 0; i < ele; i++) {
-  temp.push(1);
-  }
-  return temp;
-  });
-  // 填充newMockData数据里的bar作为格子数
-  bars.forEach((bar, i) => {
-  this.newMockData.propertion[i].bars = bar;
-  });
+    // angular想遍历数字 ===》要通过转成数组方式
+    bars = bars.map(ele => {
+      const temp = [];
+      for (let i = 0; i < ele; i++) {
+        temp.push(1);
+      }
+      return temp;
+    });
+    // 填充newMockData数据里的bar作为格子数
+    bars.forEach((bar, i) => {
+      this.newMockData.propertion[i].bars = bar;
+    });
   }
 }

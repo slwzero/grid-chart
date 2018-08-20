32a3a83d07805509a1dfaa4bcf2c5d5651b0720a
slwzero <slwzero2498@gmail.com>
2018-08-20 11:13:18 +0800

    optimize codes

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -26,41 +26,6 @@ export class ChartComponent implements OnInit {
       ele.cent = ((ele.num / ele.total) * 100).toFixed(0);
     });
   }
-  // setBar() {
-  //   // [10.5, 20.3, 19.2]
-  //   const barsAssign = this.newMockData.propertion.map(ele => {
-  //     return ele.num / ele.total * this.totalBars;
-  //   });
-  //   console.log(barsAssign);
-  //   let bars = []; // [10, 20, 19];
-  //   const points = []; // [0.5, 0.3, 0.2];
-  //
-  //   barsAssign.forEach(ele => {
-  //     points.push(ele - parseInt(ele, 10));
-  //     bars.push(parseInt(ele, 10));
-  //   });
-  //   // 判断是否是有小数，没有就返回
-  //   const maxPoint = Math.max(...points);
-  //   if (maxPoint === 0) {
-  //     return;
-  //   }
-  //   // 获取最大的小数位在points的索引值;进1位，
-  //   const index = points.indexOf(maxPoint);
-  //   bars[index] = bars[index] + 1;  // [11, 20, 19];
-  //   // angular想遍历数字 ===》要通过转成数组方式
-  //   bars = bars.map(ele => {
-  //     const temp = [];
-  //     for (let i = 0; i < ele; i++) {
-  //       temp.push(1);
-  //     }
-  //     return temp;
-  //   });
-  //   // 填充newMockData数据里的bar作为格子数
-  //   bars.forEach((bar, i) => {
-  //     this.newMockData.propertion[i].bars = bar;
-  //   });
-  //   console.log(bars);
-  // }
   setBar() {
     // [10.5, 20.3, 19.2]
     const barsAssign = this.newMockData.propertion.map(ele => {
